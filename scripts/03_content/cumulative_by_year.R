f877256333bb7424bdebba20d3c12dd4db56aa1d
Jorge Schmidt <git@jorgeschmidt.com>
2025-09-04 10:08:46 -0400

    Script to plot cumulative heat by year

---
+++ scripts/03_content/cumulative_by_year.R
@@ -0,0 +1,18 @@
+# build a graph
+
+# load packages
+library(ggplot2)
+library(EVR628tools)
+
+# load data
+data("data_heatwaves")
+
+# build a plot
+p <- ggplot(data_heatwaves,
+       mapping = aes(x = year, y = mhw_int_cumulative,
+                     group = paste(fishery, eu_rnpa))) +
+  geom_col()
+
+# export my plot
+ggsave(plot = p,
+       filename = "results/img/cumul_bar.png")
