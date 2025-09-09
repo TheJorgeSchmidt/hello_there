f4503279697e0fb79c2420ac426256ca3f2a68af
Jeynaliz.Ortiz <jeynaog19@gmail.com>
2025-09-09 09:22:53 -0400

    Assigning data heatwaves to x variable. In plot, changed y variable to be mean temperature. Code does not run. Troubleshoot

--- scripts/03_content/cumulative_by_year.R
+++ scripts/03_content/cumulative_by_year.R
@@ -5,14 +5,17 @@ library(ggplot2)
 library(EVR628tools)
 
 # load data
-data("data_heatwaves")
+x<-data("data_heatwaves")
+x
 
 # build a plot
-p <- ggplot(data_heatwaves,
-       mapping = aes(x = year, y = mhw_int_cumulative,
-                     group = paste(fishery, eu_rnpa))) +
-  geom_col()
+
+p <- ggplot(data_heatwaves, mapping = aes(x = year, y = temp_mean,
+                                          group = paste(fishery, eu_rnpa))) + geom_col()
+
+p
 
 # export my plot
 ggsave(plot = p,
        filename = "results/img/cumul_bar.png")
+
